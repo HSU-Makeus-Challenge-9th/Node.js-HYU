17b5a2103408c829ad4256190eb3e9ac58facaba
kdjidkr <kcwidrk@naver.com>
2025-11-11 01:43:54 +0900

    feat: create Week7 Mission1

--- Week7/kdjidkr/src/repositories/mission.repository.js
+++ Week7/kdjidkr/src/repositories/mission.repository.js
@@ -1,9 +1,12 @@
 import {prisma} from "../db.config.js"
+import { StoreNotFoundError } from "../errors.js";
+import { MissionNotFoundError, AlreadyChallengedMissionError } from "../errors.js";
+import { UserNotFoundError } from "../errors.js";
 
 export const addMission = async (data) => {
     const store = await prisma.store.findFirst({where: {storeId: data.storeId}});
     if (!store) {
-        return null;
+        throw new StoreNotFoundError();
     } // 존재하지 않는 가게에 대한 미션 작성 시 null 반환 후 종료
 
     const result = await prisma.mission.create({data: data});
@@ -11,6 +14,17 @@ export const addMission = async (data) => {
 }
 
 export const challengeMission = async (data) => {
+    try {
+    const user = await prisma.account.findFirst({where: {userId: data.userId}});
+    if (!user) {
+        throw new UserNotFoundError();
+    }
+
+    const mission = await prisma.mission.findFirst({where: {missionId: data.missionId}});
+    if (!mission) {
+        throw new MissionNotFoundError();
+    }
+
     const checkedMission = await prisma.userMission.findFirst({
         where: {
             missionId: data.missionId,
@@ -18,8 +32,8 @@ export const challengeMission = async (data) => {
         }
     });
     if (checkedMission) {
-        return null;
-    } // 이미 도전한 미션일 경우 null 반환 후 종료
+        throw new AlreadyChallengedMissionError();
+    } // 이미 도전한 미션일 경우 예외 발생
 
     // 시간 설정 (7일 뒤에 만료 limited_at)
     const limitedAt = new Date();
@@ -38,12 +52,21 @@ export const challengeMission = async (data) => {
         
     });
     return result
+} catch (err) {
+    if (err.statusCode) throw err;
+    throw new InternalServerError(`미션 도전 중 오류가 발생했습니다.: ${err.message}`);
+  }
 }
 
 export const getMission = async (mission_id) => {
+    try {
     const mission = await prisma.mission.findFirst({where: {missionId: mission_id}});
     if (!mission) {
-        return null;
+        throw new MissionNotFoundError();
     }
     return mission;
+} catch (err) {
+    if (err.statusCode) throw err;
+    throw new InternalServerError(`미션 조회 중 오류가 발생했습니다.: ${err.message}`);
+  };
 };
